4b173861e28bee2b68b29e0d66198ae3aab849fc
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-24 20:25:23 +0700

    add array-4

--- code/03-array/array4.js
+++ code/03-array/array4.js
@@ -1,1 +1,11 @@
-// fill, flat, flatMap, reverse
+// flat, flatMap, reverse
+const numbers = [[1], [2], [3], [4]];
+
+const numberFlat = numbers.flat();
+
+const sentences = ['JavaScript Array flatMap()', 'is a', 'Awesome'];
+
+const wordsMap = sentences.map((s) => s.split(' '));
+const wordsFlatMap = sentences.flatMap((s) => s.split(' '));
+
+const reverseWords = [...wordsFlatMap].reverse();
