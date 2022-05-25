13df7a28ee4034488b942ab8838c7368154cd033
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-25 11:03:10 +0700

    add content var, const let

--- code/01-principle/scope.js
+++ code/01-principle/scope.js
@@ -1,14 +1,27 @@
 function upper() {
-  const name = 'one';
+  let name = 'john';
 
   function inner() {
-    const name = 'two';
-    console.log('inner', name);
+    name = 'debby';
+    console.log('innner ->', name);
   }
 
   inner();
 
-  console.log('upper ', name);
+  console.log('upper ->', name);
 }
 
 upper();
+
+// function main() {
+//   const fruit = 'apple';
+
+//   if (true) {
+//     const fruit = 'banana';
+//     console.log('in if -->', fruit);
+//   }
+
+//   console.log('aftter if -->', fruit);
+// }
+
+// main();
