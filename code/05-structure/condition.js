de67450cb3063645259aec5d2033d2e3c7fbb013
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-05-25 18:06:19 +0700

    add content 05

---
+++ code/05-structure/condition.js
@@ -0,0 +1,15 @@
+// sex = MEN, age >= 21
+
+const MEN = 'M';
+const WOMEN = 'W';
+const NONE = 'N';
+
+const people = {
+  sex: MEN,
+  age: 5,
+  health: true,
+};
+
+const canConscription = () => {};
+
+main();
--- code/05-structure/function.js
+++ code/05-structure/function.js
@@ -1,5 +1,14 @@
-// if else
-
-// switch case
-
 // declarative vs imperative
+const doubleFnImperative = (numbers) => {
+  const doubled = [];
+  for (let i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+
+  return doubled;
+};
+
+const doubleDeclarative = (numbers) => numbers.map((n) => n * 2);
+
+console.log('Imperative ->', doubleFnImperative([2, 3, 4]));
+console.log('Declarative ->', doubleDeclarative([2, 3, 4]));
